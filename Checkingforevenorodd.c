16a861a0f565fe821685d3775f671d113ba50efc
yashchem08-bit <yashchem08@gmail.com>
2026-02-13 11:39:39 +0530

    Add files via upload

---
+++ Checkingforevenorodd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>// checking for even odd
+int main(){
+    int a ;
+     printf("Enter the value of a ::");
+    scanf("%d",&a);
+     
+    
+    if(a %2 == 0){
+    printf(" Number is Even ");
+    }else{
+        printf("number is odd");
+    }
+}
