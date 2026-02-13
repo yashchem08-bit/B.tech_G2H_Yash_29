747d675273106cd4ebf18a15ba79a3a1bc5f4414
yashchem08-bit <yashchem08@gmail.com>
2026-02-13 09:42:22 +0530

    Add files via upload
    This program helps us to calculate the area of circle , with taking input through user.

---
+++ areaofcircle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    float r;
+    float c;
+    float a;
+    printf("I am in betch-G2H\n");
+    printf("Enter the value of radius\n");
+    scanf("%f",&r);
+    c = 2*(3.1415)*r;
+    a=(3.1415)*r*r;
+    printf("The circumference is %f",c);
+    printf("tHE AREA is %f",a);
+    return 0;
+
+
+}
