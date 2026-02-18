72887d5f9ad12d88273b84aee61a765d51336e2c
yashchem08-bit <yashchem08@gmail.com>
2026-02-18 20:17:28 +0530

    Add files via upload

---
+++ checkForvowelorconsonent.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    char ch ;
+    printf("Enter the character ");
+    scanf("%c",&ch);
+    if(ch =='a'|| ch == 'e'||ch =='i'||ch =='o'||ch =='u'||ch =='A'||ch =='E'||ch =='I'||ch =='O'||ch =='U'){
+        printf("vowel");
+    }else{
+        printf("consonent");
+    }
+}
