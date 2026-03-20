e22cfc11fa451a5c7987378e1be19622c7f15996
yashchem08-bit <yashchem08@gmail.com>
2026-03-20 10:20:55 +0530

    Add files via upload

---
+++ reverseaninteger.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n ;
+    printf("Enter n :: ");
+    scanf("%d",&n);
+    int temp = n;
+    int count = 0;
+    while(n >0){
+        count ++;
+        n /= 10;
+    }
+    int rev = 0;
+    for(int i = count; i>0;i--){
+        int ld = temp%10;
+        rev += ld*pow(10,i-1);
+        temp /= 10;
+    }
+    printf("reverse integer is :: %d",rev);
+}
