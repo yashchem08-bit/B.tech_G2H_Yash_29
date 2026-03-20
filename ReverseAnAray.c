d15a27746afd224ac615fe995374be5f0169fda7
yashchem08-bit <yashchem08@gmail.com>
2026-03-20 09:38:46 +0530

    Add files via upload

---
+++ ReverseAnAray.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    int revarr[5];
+
+    for(int i = 5;i>0;i--){
+     revarr[5-i] = arr[i];
+
+    }
+    for(int i = 0; i<5;i++){
+        printf("%d",revarr[i]);
+    }
+    return 0;
+}
